2989a6d80f432c42c93b8b8a1f608fad86e282c4
Huong Nguyen <huongnguyen.nth1709@gmail.com>
2021-08-25 18:57:15 +0300

    fix errors

--- server.js
+++ server.js
@@ -1,27 +1,7 @@
-// const jsonServer = require('json-server')
-// const app = jsonServer.create()
-// const path = require('path')
-// const express = require('express')
-// const middlewares = jsonServer.defaults()
-// const router = jsonServer.router('anecdotes.json')
-// const PORT = process.env.PORT || 3000
-
-// app.use('/db', middlewares, router)
-// app.use(express.static(path.join(__dirname, 'build')))
-
-// app.get('/*', function (req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'))
-// })
-
-// app.listen(PORT, () => {
-//   console.log('server started on port 3000')
-// })
-
-
-const jsonServer = require('json-server')
-const app = jsonServer.create()
-const router = jsonServer.router('anecdotes.json')
-const middlewares = jsonServer.defaults({
+import { create, router as _router, defaults } from 'json-server'
+const app = create()
+const router = _router('anecdotes.json')
+const middlewares = defaults({
   static: './build'
 })
 const PORT = process.env.PORT || 3000
